e486a51ad786d72072b21cc906ad2860aeb37818
merab7 <merabtodua7@gmail.com>
2025-08-03 04:36:13 +0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
+
+
+
+function App() {
+  return (
+    <seaction>
+      <Header />
+      <Footer />
+    </seaction>
+  )
+}
+
+export default App
+
---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer>
+      <p className="text-3xl font-bold underline">
+        &copy; {new Date().getFullYear()} this is the curren year comming from this date object
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
